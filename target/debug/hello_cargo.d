11889cfc25232679b0dcc389aa3d1c9776580738
Bernard Maina <bmaina@deloitte.co.ke>
2025-04-16 16:20:56 +0300

    rust matery

---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\bmaina\source\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\bmaina\source\projects\hello_cargo\src\main.rs
---
+++ target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\bmaina\source\projects\hello_cargo\target\release\hello_cargo.exe: C:\Users\bmaina\source\projects\hello_cargo\src\main.rs
