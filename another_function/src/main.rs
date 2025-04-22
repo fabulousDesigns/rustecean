f0663160ee3bc1aa1d45ae8d593011fb6849f242
Bernard Maina <bmaina@deloitte.co.ke>
2025-04-22 15:56:09 +0300

    rustacean

--- another_function/src/main.rs
+++ another_function/src/main.rs
@@ -1,7 +1,20 @@
-fn main() {
-    print_labeled_measurement(5, 'h');
-}
-
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+fn five() -> i32 {
+    5
+}
+fn main() {
+    let y = {
+        let x = 1;
+        x + 1
+    };
+    {
+        let x = 1;
+        let y = 1;
+        let z = x + y;
+    }
+    let x: i32 = five();
+    println!("The value of x is: {x}");
+    // println!("The value of y is: {}", y);
 }
