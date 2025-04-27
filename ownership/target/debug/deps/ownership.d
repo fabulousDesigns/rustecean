2230af25dd2214cb6240570ba218bb96d42ab7c8
Bernard Maina <hazardmburu10@gmail.com>
2025-04-27 14:23:12 +0300

    Ownership

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+C:\Users\user\RustroverProjects\rustecean\ownership\target\debug\deps\ownership.exe: src\main.rs
+
+C:\Users\user\RustroverProjects\rustecean\ownership\target\debug\deps\ownership.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\user\RustroverProjects\rustecean\ownership\target\debug\ownership.exe: C:\Users\user\RustroverProjects\rustecean\ownership\src\main.rs
