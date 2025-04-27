f345927d21059272c3a11453f7a18bc342366dd1
Bernard Maina <hazardmburu10@gmail.com>
2025-04-27 09:41:01 +0300

    Ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -22,4 +22,19 @@ fn main() {
     // We need a way to return this memory to the operating system when we're done with our String.
     // In Rust, the memory is automatically returned once the variable that owns it goes out of scope:
     // When a variable goes out of scope, Rust calls a special function called drop automatically, and it's where the author of String can put the code to return the memory.
+    // Move Semantics
+    // Multiple variables can interact with the same data in different ways. Let's look at an example with an integer:
+    let x = 5;
+    let y = x;
+    // Here, we bind the value 5 to x, then make a copy of that value and bind it to y. Now we have two variables, both equal to 5. This works because integers are simple values with a known, fixed size, and these values are pushed onto the stack.
+    // But what happens with the String type?
+    let s1 = String::from("hello");
+    let s2 = s1;
+    // Clone
+    // If we actually do want to deeply copy the heap data of a String and not just the stack data, we can use a common method called clone:
+    let s3 = s2.clone();
+    println!("s2 = {}, s3 = {}", s2, s3);
+    // When you see a call to clone, you know some arbitrary code is being executed and that code may be expensive. It's a visual indicator that something different is going on.
+    // Stack-Only Data: Copy
+    // Now let's look at this code with integers:
 }
