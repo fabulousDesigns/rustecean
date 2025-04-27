dac13c25464900a8abc047f3f742163ab5bc1184
Bernard Maina <hazardmburu10@gmail.com>
2025-04-27 11:05:49 +0300

    Ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -53,7 +53,13 @@ fn main() {
     // println!("{}", greetings); // can't use a value after it has been moved
     let test_makes_copy = 5;
     makes_copy(test_makes_copy);
-    println!("{test_makes_copy}")  // Nothing happens because Rust will allow copy
+    println!("{test_makes_copy}"); // Nothing happens because Rust will allow copy
+    // Return Values and Scope
+    // Returning values can also transfer ownership. Here's an example:
+    let s4 = gives_ownership();         // gives_ownership moves its return
+    // value into s1
+    let s5 = String::from("hello");     // s2 comes into scope
+    let s6 = takes_and_gives_back(s2);  // s2 is moved into
 }
 
 fn takes_ownership(some_string: String) {
@@ -62,4 +68,13 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
+}
+
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into the scope
+    a_string // a_string is returned and moves out to the calling function
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
 }
