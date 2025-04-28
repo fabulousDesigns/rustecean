7cf89b197f4e2b3a24284906da39766ae27564ad
Bernard Maina <hazardmburu10@gmail.com>
2025-04-28 17:21:16 +0300

    structs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,7 +4,17 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn height(&self) -> bool {
+        self.height > 0
+    }
+}
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -22,7 +32,9 @@ fn main() {
         height: 50,
     };
     println!("Rectangle2: {:#?}", rectangle2);
-    println!("The area of the rectangle is {} square pixels", area_refactoring_with_struct(&rectangle2))
+    // println!("The area of the rectangle is {} square pixels",
+    // area_refactoring_with_struct(&rectangle2))
+    println!("The area of the rectangle is {} square pixels", rectangle2.area());
 }
 fn area(width: u32, height: u32) -> u32 {
     width * height
@@ -31,6 +43,6 @@ fn area_refactoring_with_struct_tuple(rectangle: (u32, u32)) -> u32 {
     let (width, height) = rectangle;
     width * height
 }
-fn area_refactoring_with_struct(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area_refactoring_with_struct(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
