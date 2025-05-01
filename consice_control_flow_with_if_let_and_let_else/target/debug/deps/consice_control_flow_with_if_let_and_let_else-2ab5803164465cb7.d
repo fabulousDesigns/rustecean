3ad431cf8cd4c1868233751257af567d8413ac71
Bernard Maina <hazardmburu10@gmail.com>
2025-05-01 10:34:45 +0300

    Enum type

---
+++ consice_control_flow_with_if_let_and_let_else/target/debug/deps/consice_control_flow_with_if_let_and_let_else-2ab5803164465cb7.d
@@ -0,0 +1,5 @@
+C:\Users\user\RustroverProjects\rustecean\consice_control_flow_with_if_let_and_let_else\target\debug\deps\libconsice_control_flow_with_if_let_and_let_else-2ab5803164465cb7.rmeta: src\main.rs
+
+C:\Users\user\RustroverProjects\rustecean\consice_control_flow_with_if_let_and_let_else\target\debug\deps\consice_control_flow_with_if_let_and_let_else-2ab5803164465cb7.d: src\main.rs
+
+src\main.rs:
---
+++ consice_control_flow_with_if_let_and_let_else/target/debug/deps/consice_control_flow_with_if_let_and_let_else-df3501872077b74b.d
@@ -0,0 +1,5 @@
+C:\Users\user\RustroverProjects\rustecean\consice_control_flow_with_if_let_and_let_else\target\debug\deps\libconsice_control_flow_with_if_let_and_let_else-df3501872077b74b.rmeta: src\main.rs
+
+C:\Users\user\RustroverProjects\rustecean\consice_control_flow_with_if_let_and_let_else\target\debug\deps\consice_control_flow_with_if_let_and_let_else-df3501872077b74b.d: src\main.rs
+
+src\main.rs:
