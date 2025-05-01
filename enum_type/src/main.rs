0b7f6cbe41a46530370f4aebd4f444d0b45de5f5
Bernard Maina <hazardmburu10@gmail.com>
2025-05-01 11:40:45 +0300

    Enum type

--- enum_type/src/main.rs
+++ enum_type/src/main.rs
@@ -18,9 +18,46 @@ enum IpAddrKind {
 fn route(ip_kind: IpAddrKind) {
     println!("{:?}", ip_kind);
 }
+// Adding data to enums
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+// This works but is verbose - we're using a struct that contains the enum.
+// Approach 2
+#[derive(Debug)]
+enum IpAddrEnum {
+    V4(String),
+    V6(String)
+}
+impl IpAddrEnum {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+// approach 3
+#[derive(Debug)]
+enum IpAddressEnum{
+    V4(u8, u8, u8, u8),
+    V6(String)
+}
 fn main() {
-    // println!("{:?}", IpAddrKind::V4);
-    // println!("{:?}", IpAddrKind::V6);
+    println!("{:?}", IpAddrKind::V4);
+    println!("{:?}", IpAddrKind::V6);
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    println!("{:?}", home.kind);
+    println!("{:?}", home.address);
+    let address_v4 = IpAddrEnum::V4(String::from("127.0.0.1"));
+    let address_v6 = IpAddrEnum::V6(String::from("::1"));
+    println!("address_v4: {:?}, address_v6: {:?}", address_v4, address_v6);
+    let ip_addr_enum_v4 = IpAddressEnum::V4(127,0,0,1);
+    let ip_addr_enum_v6 = IpAddressEnum::V6(String::from("::1"));
+    println!("ip_addr_enum_v4: {:?}", ip_addr_enum_v4);
+    println!("ip_addr_enum_v6: {:?}", ip_addr_enum_v6);
 }
